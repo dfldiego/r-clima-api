ba1ec43d276a2cc33cce54bf8c6852746fce7b6d
dfldiego <dfl.diego92@gmail.com>
2020-08-26 13:46:20 -0300

    finalizando el formulario y state creando el state y leyendo lo que el usuario agrega.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+
+    //state de busqueda
+    const [busqueda, setBusqueda] = useState({
+        ciudad: '',
+        pais: ''
+    });
+
+    const { ciudad, pais } = busqueda;
+
+    //funcion que coloca lo ingresado por usuario en el state
+    const handleChange = e => {
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
         <form>
             <div className="input-field col s12">
@@ -8,6 +25,8 @@ const Formulario = () => {
                     type="text"
                     name="ciudad"
                     id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
                 />
                 <label htmlFor="ciudad">Ciudad: </label>
             </div>
@@ -16,8 +35,17 @@ const Formulario = () => {
                 <select
                     name="pais"
                     id="pais"
+                    value={pais}
+                    onChange={handleChange}
                 >
                     <option value="">-- Seleccione un país --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
                 </select>
             </div>
         </form>
